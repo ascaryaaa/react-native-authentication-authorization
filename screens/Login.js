71a9a808fa9e7008bf8e791bd1eb948638562eec
ascaryaaa <allaamascarya@gmail.com>
2024-02-29 10:11:08 +0700

    Added Firebase Authentication Functions: Login and Registration Handlers

--- screens/Login.js
+++ screens/Login.js
@@ -1,17 +1,37 @@
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native"
 import { useState } from "react"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { FIREBASE_APP, FIREBASE_AUTH } from "../helpers/firebase";
 
 const Login = () => {
+    const auth=FIREBASE_AUTH
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    const handleLogin = async () => {
+        try {
+            const response = await signInWithEmailAndPassword(auth, email, password)
+                .then(res => console.log(res))
+                .catch(err => console.log(err));
+        } catch (error) {
+
+        }
+    }
+
+    const handleRegister = async() => {
+        try {
+            const response = await createUserWithEmailAndPassword(auth, email, password)
+            console.log(response)
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Login</Text>
             <TextInput 
                 style={styles.input} 
-                placeholder="username"
-                secureTextEntry
+                placeholder="email"
                 value={email}
                 onChangeText={setEmail}
             />
@@ -21,8 +41,11 @@ const Login = () => {
                 value={password}
                 onChangeText={setPassword}    
             />
-            <TouchableOpacity style={styles.buttonLogin}>
-                <Text>Login</Text>
+            <TouchableOpacity title='login' style={styles.button} onPress={handleLogin}>
+                <Text style={{color:'white'}} >Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity title='register' style={styles.button} onPress={handleRegister}>
+                <Text style={{color:'white'}} >Register</Text>
             </TouchableOpacity>
         </View>
     )
@@ -39,13 +62,22 @@ const styles = StyleSheet.create({
         borderColor: '#ccc',
         borderRadius: 8,
         paddingVertical: 5,
-        paddingHorizontal: 25,
+        paddingHorizontal: 15,
         marginBottom: 16,
         fontSize: 16,
         color: '#333',
+        width: 200,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
-    buttonLogin: {
-        backgroundColor: 'pink',
+    button: {
+        backgroundColor: '#4287f5',
+        padding: 5,
+        paddingHorizontal: 15,
+        borderRadius: 5,
+        width: 100,
+        alignItems: 'center',
+        margin: 5,
     }
 });
 
