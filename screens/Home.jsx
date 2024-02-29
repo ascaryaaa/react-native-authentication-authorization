c15fac9e4f16bf86b91c97811d074f093e8428e6
ascaryaaa <allaamascarya@gmail.com>
2024-02-29 14:20:45 +0700

    Implemented API Fetching based on Authorization Status

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,7 +1,23 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native"
+import { useState, useEffect } from "react"
 
 const Home = ({navigation}) => {
+    const [animes, setAnimes] = useState([])
+
+    useEffect(() => {
+        AsyncStorage
+        .getItem('token') //check token existed
+        .then(token => {
+            if (token !== null) { //if token existed
+                return fetch('https://api.jikan.moe/v4/top/anime')
+            }
+            return Promise.reject('Not Authorize') //if token not existed
+        })
+        .then(response => response.json()) //convert response token
+        .then(({data}) => setAnimes(data))
+    },[])
+    
     const handleLogout = () => {
         AsyncStorage
         .removeItem('token')
@@ -11,6 +27,7 @@ const Home = ({navigation}) => {
     return (
         <View style={styles.container}>
             <Text>Home</Text>
+            {animes.map(({title}) => <Text>{title}</Text>)}
             <TouchableOpacity title='login' style={styles.button} onPress={handleLogout}>
                 <Text style={{color:'white'}} >Logout</Text>
             </TouchableOpacity>
