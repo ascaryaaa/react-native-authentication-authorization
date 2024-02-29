e88f9b7aba7af6cbabab40107a883d3149b927e4
ascaryaaa <allaamascarya@gmail.com>
2024-02-29 09:11:11 +0700

    added login screen to stack navigation

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './screens/Login';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +10,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        
+        <Stack.Screen 
+          name="Login" 
+          component={Login} 
+          options={{
+            title:'Login'
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
